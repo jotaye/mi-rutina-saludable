356dd6caa60628442cafd9aa1594e11252181325
jotaye <jotayegroupllc@gmail.com>
2025-06-04 13:20:17 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+// tailwind.config.js
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: "class", // usamos la clase "dark" para el modo oscuro
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
