4a864a666bb1f1535e22f739a093c9f08c7bc6e6
jotaye <jotayegroupllc@gmail.com>
2025-06-04 18:03:14 -0400

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 // src/pages/Login.jsx
-
 import React, { useState, useContext } from "react";
 import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -30,9 +29,7 @@ export default function Login() {
         <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">
           {lang === "es" ? "Iniciar sesión" : "Log In"}
         </h2>
-
         {errorMsg && <p className="text-red-500 text-sm mb-2">{errorMsg}</p>}
-
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block text-gray-700">
@@ -65,7 +62,6 @@ export default function Login() {
             {lang === "es" ? "Ingresar" : "Log In"}
           </button>
         </form>
-
         <p className="text-center text-sm text-gray-500 mt-4">
           {lang === "es" ? "¿No tienes cuenta?" : "Don’t have an account?"}{" "}
           <a href="/registro" className="text-blue-600 hover:underline">
