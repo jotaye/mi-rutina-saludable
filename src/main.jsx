f2783a74449e7c668b3dfe7c2238758de76f8396
jotaye <jotayegroupllc@gmail.com>
2025-06-04 17:59:20 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 // src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Asegura que Tailwind se cargue
+import "./index.css"; // Carga TailwindCSS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
