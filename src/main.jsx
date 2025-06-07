a3fd5e84f99a49d48acd92f92a62582bea029154
jotaye <jotayegroupllc@gmail.com>
2025-06-07 03:09:19 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
 // src/main.jsx
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(regs => regs.forEach(r => r.unregister()))
+    .catch(console.error);
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-// Registra el service worker generado por vite-plugin-pwa
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => {
-        console.log("Service worker registrado:", reg);
-      })
-      .catch((err) => {
-        console.error("Error al registrar service worker:", err);
-      });
-  });
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
