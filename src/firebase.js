40c4a9d611053d559d7e34d5d80712449ca63470
jotaye <jotayegroupllc@gmail.com>
2025-06-07 04:17:09 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,25 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+-import { initializeApp } from "firebase/app";
+-import { getAuth } from "firebase/auth";
++import { initializeApp } from "firebase/app";
++import { getAuth } from "firebase/auth";
++import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC7jYi0ST5rfYvfZcb8QgeMmvvVcrKDFiU",
-  authDomain: "mi-rutina-saludable.firebaseapp.com",
-  projectId: "mi-rutina-saludable",
-  storageBucket: "mi-rutina-saludable.firebasestorage.app",
-  messagingSenderId: "17810301001",
-  appId: "1:17810301001:web:a0d9b260b138c81980df98",
-  measurementId: "G-W2ZMDYK46E"
-};
+ const firebaseConfig = {
+   apiKey: "AIzaSyC7jYi0ST5rfYvfZcb8QgeMmvvVcrKDFiU",
+   authDomain: "mi-rutina-saludable.firebaseapp.com",
+   projectId: "mi-rutina-saludable",
+   storageBucket: "mi-rutina-saludable.firebasestorage.app",
+   messagingSenderId: "17810301001",
+   appId: "1:17810301001:web:a0d9b260b138c81980df98",
+   measurementId: "G-W2ZMDYK46E"
+ };
 
-// Inicializa la app una sola vez
-const app = initializeApp(firebaseConfig);
-// Exporta el auth ya inicializado
-export const auth = getAuth(app);
+ // Inicializa Firebase App
+ const app = initializeApp(firebaseConfig);
+
+ // Exporta Auth
+ export const auth = getAuth(app);
+
+ // <<< Nuevo: inicializa y exporta Firestore
+ export const db = getFirestore(app);
