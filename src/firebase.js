a467ea82f67b19f24e1f85ac6707b821051fc892
jotaye <jotayegroupllc@gmail.com>
2025-06-06 21:39:43 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7jYi0ST5rfYvfZcb8QgeMmvvVcrKDFiU",
+  authDomain: "mi-rutina-saludable.firebaseapp.com",
+  projectId: "mi-rutina-saludable",
+  storageBucket: "mi-rutina-saludable.firebasestorage.app",
+  messagingSenderId: "17810301001",
+  appId: "1:17810301001:web:a0d9b260b138c81980df98",
+  measurementId: "G-W2ZMDYK46E"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
